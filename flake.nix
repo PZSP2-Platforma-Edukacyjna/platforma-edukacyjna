f02d42686c4c9b62791affcc064b1932f538bc8f
Sebastian Wojciechowski <01191885@pw.edu.pl>
2026-04-10 11:29:44 +0200

    initial backend structure

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { self, nixpkgs, flake-parts }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { pkgs, ... }: let
+        python = pkgs.python314.withPackages(ps: with ps; [
+          django_6
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.pyright
+          ];
+        };
+      };
+    });
+}
