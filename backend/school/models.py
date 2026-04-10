679d290ec3837bcbe5c69e27c14c65301cff53ef
Sebastian Wojciechowski <01191885@pw.edu.pl>
2026-04-10 12:30:01 +0200

    add course related tables

---
+++ backend/school/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from users.models import User
+
+class Student(models.Model):
+    parent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='students', limit_choices_to={'role': User.Role.PARENT})
+    pesel = models.CharField(max_length=11)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Course(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_taught', limit_choices_to={'role': User.Role.TEACHER})
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    students = models.ManyToManyField('Student', related_name='enrolled_courses', blank=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+    topic = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return str(self.topic)
