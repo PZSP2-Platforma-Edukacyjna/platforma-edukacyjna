7a3afe692db556b1f7c00f0f23af5635660496bb
mzielin1 <01178617@pw.edu.pl>
2026-04-11 13:24:53 +0200

    Schedule layout modifications

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,14 +1,23 @@
+import TopBar from "@/components/layout/TopBar";
+import ScheduleGrid from "@/components/schedule/ScheduleGrid";
+import SubjectsList from "@/components/subjects/SubjectsList";
+import MessagesList from "@/components/messages/MessagesList";
+import NewsList from "@/components/news/NewsList";
+
 export default function Dashboard() {
   return (
-    <div className="h-screen flex flex-col bg-white">
+    <div className="h-screen flex flex-col">
       <TopBar />
 
-      <div className="flex flex-1 gap-4 p-2">
+      <div className="flex flex-1 gap-4 p-4">
+        {/* LEWA CZĘŚĆ */}
         <div className="flex flex-col flex-[4] gap-4">
-          <div className="flex-[2] border rounded p-1 overflow-auto">
+          {/* PLAN */}
+          <div className="flex-[2] border rounded p-2 overflow-auto">
             <ScheduleGrid />
           </div>
 
+          {/* DÓŁ */}
           <div className="flex flex-[1] gap-4">
             <div className="flex-1">
               <NewsList />
@@ -20,7 +29,8 @@ export default function Dashboard() {
           </div>
         </div>
 
-        <div className="flex-[1] max-w-[20%] pr-2">
+        {/* PRAWA KOLUMNA (WĄSKA) */}
+        <div className="flex-[1] max-w-[20%]">
           <SubjectsList />
         </div>
       </div>
